d1984286843de199ab6f817eb0b799c1f14538d9
AlbanMarku <albanmarku@outlook.com>
2022-07-01 12:36:40 +0100

    Added standard reload function.

---
+++ main.py
@@ -0,0 +1,23 @@
+def standardReload(a,b,c):
+    ammoReserve = a
+    ammoCap = b
+    ammoLoaded = c
+    if ammoCap == ammoLoaded:
+        print("Gun already loaded")
+    else:
+        print("CHANGING!!!")
+        ammoNeeded = ammoCap - ammoLoaded
+        if ammoNeeded < ammoReserve:
+            ammoReserve = ammoReserve - ammoNeeded
+            ammoLoaded = ammoLoaded + ammoNeeded
+        else:
+            ammoLoaded = ammoLoaded + ammoReserve
+            ammoReserve = 0
+        print("Ammo remaining is " + str(ammoReserve) + " and your mag is loaded to " + str(ammoLoaded))
+        
+        
+res = int(input('res?'))
+cap = int(input('cap?'))
+lod = int(input('lod?'))
+
+standardReload(res,cap,lod)
