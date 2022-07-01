ab5ed3616f93a2846a0a014f5bb49ccce00e9cf8
AlbanMarku <albanmarku@outlook.com>
2022-07-01 17:58:34 +0100

    Cleaned up repetitive code.

--- main.py
+++ main.py
@@ -3,57 +3,52 @@ def standardReload(a,b,c):
     ammoCap = b
     ammoLoaded = c
 
-    if ammoReserve <= 0:
-        print("Empty ammo. Can't reload.")
-    else:
+    if not canReload(ammoReserve,ammoCap,ammoLoaded):
+        print("CHANGING!!!")
+        ammoNeeded = ammoCap - ammoLoaded
 
-        if ammoCap == ammoLoaded:
-            print("Gun already loaded.")
-        else:
-            print("CHANGING!!!")
-            ammoNeeded = ammoCap - ammoLoaded
-
-            if ammoNeeded < ammoReserve:
-                
-                if ammoLoaded == 0:
-                    ammoLoaded += ammoNeeded - 1
-                    ammoReserve -=ammoNeeded -1
-                else:
-                    ammoLoaded += ammoNeeded
-                    ammoReserve -=ammoNeeded
-            else:
-                ammoLoaded += ammoReserve
-                ammoReserve -= ammoReserve
-            print("Ammo remaining is " + str(ammoReserve) + " and your mag is loaded to " + str(ammoLoaded))
+        if ammoNeeded < ammoReserve:
             
-def tacticalReload(a,b,c):
-    ammoReserve = a
-    ammoCap = b
-    ammoLoaded = c
-
-    if ammoReserve <= 0:
-        print("Empty ammo. Can't reload.")
-    else:
-
-        if ammoCap == ammoLoaded:
-            print("Gun already loaded")
-        else:
-            print("TAC RELOAD!!!")
-            ammoNeeded = ammoCap - ammoLoaded
-            if ammoCap < ammoReserve:
-                if ammoLoaded == 0:
-                    ammoLoaded = ammoCap - 1
-                    ammoReserve -= ammoCap - 1
-                else:
-                    ammoLoaded = ammoCap
-                    ammoReserve -= ammoCap - 1
+            if ammoLoaded == 0:
+                ammoLoaded += ammoNeeded - 1
+                ammoReserve -=ammoNeeded -1
             else:
-                ammoLoaded = ammoReserve
-                ammoReserve -= ammoReserve
-            print("Ammo remaining is " + str(ammoReserve) + " and your mag is loaded to " + str(ammoLoaded))
+                ammoLoaded += ammoNeeded
+                ammoReserve -=ammoNeeded
+        else:
+            ammoLoaded += ammoReserve
+            ammoReserve -= ammoReserve
+        print("Ammo remaining is " + str(ammoReserve) + " and your mag is loaded to " + str(ammoLoaded))
+            
+def tacticalReload(ammoReserve,ammoCap,ammoLoaded):
+    
+    if not canReload(ammoReserve,ammoCap,ammoLoaded):
+        print("TAC RELOAD!!!")
+        if ammoCap < ammoReserve:
+            if ammoLoaded == 0:
+                ammoLoaded = ammoCap - 1
+                ammoReserve -= ammoCap - 1
+            else:
+                ammoLoaded = ammoCap
+                ammoReserve -= ammoCap - 1
+        else:
+            ammoLoaded = ammoReserve
+            ammoReserve -= ammoReserve
+        print("Ammo remaining is " + str(ammoReserve) + " and your mag is loaded to " + str(ammoLoaded))
+
+def canReload(ammoReserve, ammoCap, ammoLoaded):
+    if ammoReserve <= 0:
+        print("No ammo. Can't reload.")
+        return True
+    elif ammoCap == ammoLoaded:       
+        print("Already loaded.")
+        return True
+    else:
+        return False
+
 
 res = int(input('res?'))
 cap = int(input('cap?'))
 lod = int(input('lod?'))
 
-standardReload(res,cap,lod)
+tacticalReload(res,cap,lod)
