e95cb9aed010f23489c15ee590f60c5ccd678dd4
AlbanMarku <albanmarku@outlook.com>
2022-07-01 17:26:48 +0100

    Added tactical reload method.

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def standardReload(a,b,c):
 
             if ammoNeeded < ammoReserve:
                 
-                if ammoLoaded ==0:
+                if ammoLoaded == 0:
                     ammoLoaded += ammoNeeded - 1
                     ammoReserve -=ammoNeeded -1
                 else:
@@ -26,7 +26,32 @@ def standardReload(a,b,c):
                 ammoReserve -= ammoReserve
             print("Ammo remaining is " + str(ammoReserve) + " and your mag is loaded to " + str(ammoLoaded))
             
-        
+def tacticalReload(a,b,c):
+    ammoReserve = a
+    ammoCap = b
+    ammoLoaded = c
+
+    if ammoReserve <= 0:
+        print("Empty ammo. Can't reload.")
+    else:
+
+        if ammoCap == ammoLoaded:
+            print("Gun already loaded")
+        else:
+            print("TAC RELOAD!!!")
+            ammoNeeded = ammoCap - ammoLoaded
+            if ammoCap < ammoReserve:
+                if ammoLoaded == 0:
+                    ammoLoaded = ammoCap - 1
+                    ammoReserve -= ammoCap - 1
+                else:
+                    ammoLoaded = ammoCap
+                    ammoReserve -= ammoCap - 1
+            else:
+                ammoLoaded = ammoReserve
+                ammoReserve -= ammoReserve
+            print("Ammo remaining is " + str(ammoReserve) + " and your mag is loaded to " + str(ammoLoaded))
+
 res = int(input('res?'))
 cap = int(input('cap?'))
 lod = int(input('lod?'))
